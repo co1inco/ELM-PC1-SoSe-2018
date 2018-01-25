1502f27ba427712da0164511a244a19565a2ec16
co1inco <colin.meihoefer@gmx.de>
2018-01-25 18:51:20 +0100

    Aufgabe 3

---
+++ PythonApplication1/Aufgabe 3/Aufgabe_3.py
@@ -0,0 +1,11 @@
+
+z1 = int(input("Zahl 1:"))
+z2 = int(input("Zahl 2:"))
+
+zs1 = str(z1)
+zs2 = str(z2)
+
+print("Die Summe von " + zs1 + " und " + zs2 + " ist: " + str(z1+z2))
+print("Die Differenz von " + zs1 + " und " + zs2 + " ist: " + str(z1-z2))
+print("Das Produkt von " + zs1 + " und " + zs2 + " ist: " + str(z1*z2))
+print("Der Quotient von " + zs1 + " und " + zs2 + " ist: " + str(z1/z2))
