5944e094dd1cfdb9b487b2ba67ad778d61f6c318
co1inco <colin.meihoefer@gmx.de>
2018-01-25 19:22:56 +0100

    Aufgabe 8

---
+++ PythonApplication1/Aufgabe 8/Aufgabe_8.py
@@ -0,0 +1,14 @@
+
+forVar = 0
+whileVar = 0
+
+for i in range(1, 101):
+    forVar = forVar + i
+
+i = 1
+while i <= 100:
+    whileVar = whileVar + i
+    i = i + 1
+
+print(forVar)
+print(whileVar)
