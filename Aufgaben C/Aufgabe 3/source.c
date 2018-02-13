4621162f6a565bfa6b98e2f4668cbbd8f5e3ef48
co1inco <colin.meihoefer@gmx.de>
2018-02-13 20:17:47 +0100

    Added describtion to input request

--- Aufgaben C/Aufgabe 3/source.c
+++ Aufgaben C/Aufgabe 3/source.c
@@ -4,10 +4,12 @@ int main(){
 	int z1;
 	int z2;
 	
-	scanf("%i", &z1);
+	printf("Zahl 1: ");
+	scanf(" %i", &z1);
+	printf("\nZahl 2: ");
 	scanf("%i", &z2);
 	
-	printf("Summe von %i und %i ist %i\n", z1, z2, z1+z2);
+	printf("\nSumme von %i und %i ist %i\n", z1, z2, z1+z2);
 	printf("Differenz von %i und %i ist %i\n", z1, z2 , z1-z2);
 	printf("Produkt von %i und %i ist %i\n", z1, z2, z1*z2);
 	printf("Quotient von %i und %i ist %i\n", z1, z2, z1/z2);
